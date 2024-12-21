cbc3c76a7e5aa853be9e1d9bb1740a9ad25a4cfe
Subodhini0906 <subodhiniaggarwal@gmail.com>
2024-12-21 22:41:42 +0530

    ..

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,10 @@ use rand::Rng;
 fn main() {  
     println!("guessing game");
 
+    let secret_num=rand::thread_rng().gen_range(1, 150);
+
     println!("pls input your guess");
+    println!("secret:{}",secret_num);
 
     let mut guess =String::new();
 
